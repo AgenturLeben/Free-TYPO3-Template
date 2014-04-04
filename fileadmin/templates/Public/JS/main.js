8cb5023ce51893184038791b3282f5d23ba1134b
baumpaul <phillip@baumg.me>
2014-04-04 11:49:48 +0200

    Update auf TYPO3 6.0 Template und Google Tag Manager

---
+++ fileadmin/templates/Public/JS/main.js
@@ -0,0 +1,8 @@
+$(function() {
+
+    $( ".csc-form-element-textarea textarea" ).click(function() {
+	  //$( ".js .csc-form-element" ).css( "display", "block" );
+	  $( ".js .csc-form-element" ).slideDown();
+	});
+
+});
