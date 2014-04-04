ea161ba3a99f2138050190ecf70253b19ef8bce0
baumpaul <phillip@baumg.me>
2014-04-04 11:58:16 +0200

    Update auf TYPO3 6.2 Template und Google Tag Manager
    This reverts commit dcdab95009a144b085805fed48eddcb9cfd7fc04.

--- fileadmin/templates/Public/JS/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-
-    $( ".csc-form-element-textarea textarea" ).click(function() {
-	  //$( ".js .csc-form-element" ).css( "display", "block" );
-	  $( ".js .csc-form-element" ).slideDown();
-	});
-
-});
